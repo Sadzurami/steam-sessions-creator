e043e907a3a14a1e7cd901c982395ba18b02d35a
Sadzurami <sadzurami2036@gmail.com>
2023-09-15 11:59:11 +0300

    fix importAll

--- src/modules/secrets/secrets.service.ts
+++ src/modules/secrets/secrets.service.ts
@@ -1,4 +1,5 @@
 import fs from 'fs/promises';
+import pQueue from 'p-queue';
 import path from 'path';
 
 import { Injectable, Logger } from '@nestjs/common';
@@ -28,7 +29,33 @@ export class SecretsService {
     return secret;
   }
 
-  public async importAll(filePath: string) {
+  public async importAll(directoryPath: string) {
+    const queue = new pQueue({ concurrency: 512 });
+
+    try {
+      await fs.access(directoryPath, fs.constants.F_OK | fs.constants.R_OK);
+    } catch (error) {
+      this.logger.verbose(`Directory ${directoryPath} does not exist or is not readable`);
+      return;
+    }
+
+    let files: string[];
+    try {
+      files = await fs.readdir(directoryPath);
+    } catch (error) {
+      this.logger.debug(new Error(`Error reading directory ${directoryPath}`), { cause: error });
+      return;
+    }
+
+    files = files.filter((file) => file.endsWith('.maFile'));
+
+    for (const file of files) queue.add(() => this.importOne(`${directoryPath}/${file}`));
+
+    await queue.onIdle();
+    this.logger.verbose(`Directory ${directoryPath} successfully imported`);
+  }
+
+  public async importOne(filePath: string) {
     if (!filePath) return;
 
     try {
